20562241c2859cd7d8fe81df2bb8d4a9342945f6
racheeta <racheeta.s.m@gmail.com>
2025-03-03 14:55:11 +0530

    semi colon problem solved
    there was syntax error caused  because of writing ";"  after @RenderBody()

--- HealthCentreCare/Models/Registration.cs
+++ HealthCentreCare/Models/Registration.cs
@@ -1,24 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;    
-namespace HealthCentreCare.Models
+﻿//using System.ComponentModel.DataAnnotations;    
+//namespace HealthCentreCare.Models
 
-{
-    public class Registration
-    {
-        [Required(ErrorMessage = "Please enter your first name")] 
-        public string Firstname { get; set; }
+//{
+//    public class Registration
+//    {
+//        [Required(ErrorMessage = "Please enter your first name")] 
+//        public string Firstname { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        public string Lastname { get; set; }
+//        [Required(ErrorMessage = "Please enter your last name")]
+//        public string Lastname { get; set; }
 
-        public string MyProperty { get; set; }
+//        public string MyProperty { get; set; }
 
-        public string MyProperty { get; set; }
-        public string MyProperty { get; set; }
+//        public string MyProperty { get; set; }
+//        public string MyProperty { get; set; }
 
-        public string MyProperty { get; set; }
+//        public string MyProperty { get; set; }
 
-        public string MyProperty { get; set; }
+//        public string MyProperty { get; set; }
 
-        public string MyProperty { get; set; }
-    }
-}
+//        public string MyProperty { get; set; }
+//    }
+//}
